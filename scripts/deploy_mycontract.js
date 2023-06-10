5c4d5a08312910fe11f15fd2b4304fe247e43caf
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2023-06-10 20:43:16 -0300

    2. Deploy Custom Smart Contracts using Hardhat

---
+++ scripts/deploy_mycontract.js
@@ -0,0 +1,33 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+ const hre = require("hardhat");
+ 
+async function main() {
+ const MyContract = await hre.ethers.getContractFactory("MyContract");
+ const myContract = await MyContract.deploy();
+ 
+ await myContract.deployed();
+ console.log("Contract address: ", myContract.address);
+}
+ 
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+ console.error(error);
+ process.exitCode = 1;
+});
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
